45e686a3948b028260545bb729915464b25a5555
Zefanja Jobse <zefanjaj@gmail.com>
2022-07-19 10:53:36 +0200

    assets

--- Battlefield rich presence/DiscordPresence.cs
+++ Battlefield rich presence/DiscordPresence.cs
@@ -84,7 +84,8 @@ namespace BattlefieldRichPresence
                     {
                         LargeImageKey = gameInfo.ShortName,
                         LargeImageText = gameInfo.GameName,
-                        SmallImageKey = gameInfo.ShortName
+                        SmallImageKey = "gt",
+                        SmallImageText = "Battlefield rich presence"
                     }
                 });
             }
