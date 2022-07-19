4f462920b46a8e3ecf3b707a6e9e00c57a1e8325
Zefanja Jobse <zefanjaj@gmail.com>
2022-07-19 23:03:06 +0200

    add support for battlefield hardline

--- Battlefield rich presence/Resources/Statics.cs
+++ Battlefield rich presence/Resources/Statics.cs
@@ -14,10 +14,11 @@ namespace BattlefieldRichPresence.Resources
             Bfbc2,
             Bf3,
             Bf4,
+            Bfh,
             Bf1
         }
         // Regex group names need to match enum keys, since we need to map the matched group to an enum value 
-        public static readonly string SupportedGamesRegex = @"^(?:(?:battlefield(?:(?'Bf1'\u2122 1)|(?'Bf3' 3\u2122)|(?'Bf4' 4)|(?'Bfbc2': bad company 2)|(?'Bfvietnam' vietnam)))|(?:bf(?:(?'Bf2'2)|(?'Bf2142'2142)) \(v1\.[\.\-0-9]+, pid: [0-9]+\))|(?'Bf1942'bf1942 \(Ver: \w{3}, \d+ \w{3} \d+ [:0-9]+\)))$";
+        public static readonly string SupportedGamesRegex = @"^(?:(?:battlefield(?:(?'Bf1'\u2122 1)|(?'Bf3' 3\u2122)|(?'Bf4' 4)|(?'Bfh' Hardline)|(?'Bfbc2': bad company 2)|(?'Bfvietnam' vietnam)))|(?:bf(?:(?'Bf2'2)|(?'Bf2142'2142)) \(v1\.[\.\-0-9]+, pid: [0-9]+\))|(?'Bf1942'bf1942 \(Ver: \w{3}, \d+ \w{3} \d+ [:0-9]+\)))$";
         public static readonly Dictionary<Game, string> ShortGameName = new Dictionary<Game, string>
         {
             { Game.Bf1942, "bf1942" },
@@ -27,6 +28,7 @@ namespace BattlefieldRichPresence.Resources
             { Game.Bfbc2, "bfbc2" },
             { Game.Bf3, "bf3" },
             { Game.Bf4, "bf4" },
+            { Game.Bfh, "bfh" },
             { Game.Bf1, "bf1" },
         };
         public static readonly Dictionary<Game, string> FullGameName = new Dictionary<Game, string>
@@ -38,6 +40,7 @@ namespace BattlefieldRichPresence.Resources
             { Game.Bfbc2, "Battlefield: Bad Company 2" },
             { Game.Bf3, "Battlefield 3" },
             { Game.Bf4, "Battlefield 4" },
+            { Game.Bfh, "Battlefield Hardline" },
             { Game.Bf1, "Battlefield 1" },
         };
         public static readonly List<Game> Frostbite3Games = new List<Game>
@@ -61,7 +64,7 @@ namespace BattlefieldRichPresence.Resources
         public static readonly List<Game> GametoolsGames = new List<Game>
         {
             Game.Bf3,
-            //Game.Bfh
+            Game.Bfh
         };
         public static readonly Dictionary<Game, string> GameClientIds = new Dictionary<Game, string>
         {
@@ -72,6 +75,7 @@ namespace BattlefieldRichPresence.Resources
             { Game.Bfbc2, "998710536919330927" },
             { Game.Bf3, "998710399975305327" },
             { Game.Bf4, "998710324922437702" },
+            { Game.Bfh, "999057759876161587" },
             { Game.Bf1, "998710285605019708" },
         };
     }
