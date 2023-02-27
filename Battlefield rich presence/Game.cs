0cf8623251d6313bb0254466dcaf82ebd32b5208
John Brechbuhl <jbrechbuhl@gmail.com>
2023-02-26 21:11:45 -0600

    fix: bf1942 game process detection

--- Battlefield rich presence/Game.cs
+++ Battlefield rich presence/Game.cs
@@ -8,24 +8,24 @@ namespace BattlefieldRichPresence
 {
     internal class Game
     {
-
         public static GameInfo IsRunning()
         {
+            Process[] bf1942Processes = Process.GetProcessesByName("BF1942");
+            if (bf1942Processes.Length == 1)
+            {
+                Statics.Game game = (Statics.Game)Enum.Parse(typeof(Statics.Game), Statics.ShortGameName[Statics.Game.Bf1942]);
+                return new GameInfo
+                {
+                    Game = game,
+                    IsRunning = true,
+                    ShortName = Statics.ShortGameName[game],
+                    FullName = Statics.FullGameName[game]
+                };
+            }
+
             Process[] processCollection = Process.GetProcesses();
             foreach (Process p in processCollection)
             {
-                if (p.ProcessName == "BF1942")
-                {
-                    Statics.Game game = (Statics.Game)Enum.Parse(typeof(Statics.Game), Statics.ShortGameName[Statics.Game.Bf1942]);
-                    return new GameInfo
-                    {
-                        Game = game,
-                        IsRunning = true,
-                        ShortName = Statics.ShortGameName[game],
-                        FullName = Statics.FullGameName[game]
-                    };
-                }
-
                 Regex rgx = new Regex(Statics.SupportedGamesRegex, RegexOptions.IgnoreCase);
                 Match match = rgx.Match(p.MainWindowTitle);
                 if (match.Success)
